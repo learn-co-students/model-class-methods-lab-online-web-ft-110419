9bfc11b1c2857bb57079df5e78b7d48f03636ab9
nkizito92 <nkizito57@gmail.com>
2020-02-10 22:52:14 -0600

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,27 +4,34 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
-    # all.limit(5)
+    self.all.limit(5).to_a
+    # doc = self.all
+    # doc[0..4]
   end
 
   def self.dinghy
-    # where("length < 20")
+     self.where("length < 20")
+    # self.all.select {|boat| boat.length < 20}
   end
 
   def self.ship
-    # where("length >= 20")
+    where("length >= 20")
+    # self.all.select {|boat| boat.length >= 20}
   end
 
   def self.last_three_alphabetically
-    # all.order(name: :desc).limit(3)
+     all.order(name: :desc).limit(3)
+
+    # self.all.sort {|boatx, boaty| -(boatx.name <=> boaty.name)}[0..2]
   end
 
   def self.without_a_captain
-    # where(captain_id: nil)
+     where(captain_id: nil)
+    # self.all.select {|boat| boat.captain_id == nil}
   end
 
   def self.sailboats
-    # includes(:classifications).where(classifications: { name: 'Sailboat' })
+     includes(:classifications).where(classifications: { name: 'Sailboat' })
   end
 
   def self.with_three_classifications
@@ -32,14 +39,15 @@ class Boat < ActiveRecord::Base
     # regularly. Just know that we can get this out of the database in
     # milliseconds whereas it would take whole seconds for Ruby to do the same.
     #
-    # joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+    joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
   end
 
   def self.non_sailboats
-    # where("id NOT IN (?)", self.sailboats.pluck(:id))
+    where("id NOT IN (?)", self.sailboats.pluck(:id))
   end
 
   def self.longest
-    # order('length DESC').first
+    order('length DESC').first
+    # self.all.sort {|x,y| -(x <=> y) }.first
   end
 end
