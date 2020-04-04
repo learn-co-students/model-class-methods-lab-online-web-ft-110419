56726936f4a44b70819a69a901640f7b41f4611c
Richell Bonfield <richell.bonfield12@gmail.com>
2020-04-04 21:41:44 +0000

    Done.

--- spec/models/captain_spec.rb
+++ spec/models/captain_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
+describe Classification do
+  describe "::my_all" do
+    it "returns all classifications" do
+      classifications = ["Ketch", "Sailboat", "Catamaran", "Sloop", "Motorboat", "Center Console", "RIB", "Trawler", "Cat Rig Boat", "Bass Boat", "Pontoon Boat"]
+      expect(Classification.my_all.pluck(:name).uniq).to eq(classifications)
+    end
+  end
+
+  describe "#longest" do
+    it "returns the classifications for the longest boat" do
+      classifications = ["Motorboat", "Center Console"]
+      expect(Classification.longest.pluck(:name)).to eq(classifications)
+    end
+  end
+end
+
+
+
+
+require 'spec_helper'
+
 describe Captain do
   describe "::catamaran_operators" do
     it "returns all captains of catamarans" do
